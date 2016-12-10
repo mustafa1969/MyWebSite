d189176f7e0179b56940b3786afd4519510055f6
DESKTOP-O3DV71L\Dell <maaayd@yahoo.com>
2016-12-10 17:37:46 -0500

    update2

---
+++ MyWork/2-eventScpe and controller/1-addingAcontroller tohandleEvent/script.js
@@ -0,0 +1,35 @@
+//code goes
+
+angular.module('app',[]);
+angular.module('app').controller('mainCtrl', function($scope){
+
+       $scope.user = {
+        name: 'Mustafa Ayad',
+        address: {
+          street: '3399 Edison Dr.',
+          city   : 'West Lafayette',
+          planet: 'earth'
+        },
+        friends:[
+        'Elyas',
+         'Mohamed',
+         'Alaa',
+         'ayat'
+        ]
+       }
+   
+  });
+
+angular.module('app').directive('userInfoCard', function(){
+
+  return {
+    templateUrl: "userInfoCard.html",
+    restrict: "E",
+    controller: function($scope){
+      $scope.knightMe = function(user){
+     user.rank ="knight";
+   }
+     
+   }
+  }
+})
